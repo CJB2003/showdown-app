ed0c1159e1f5c7eb26c6a131b8bf69bc4975e894
Chris Bunny <christopherjbunny@gmail.com>
2026-05-12 10:45:08 -0500

    Iterating through the Person array list and getting instances of person class checking if the person is a superhero or a villain.

--- src/main/java/com/pluralsight/ShowdownApp.java
+++ src/main/java/com/pluralsight/ShowdownApp.java
@@ -1,11 +1,33 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class ShowdownApp {
     public static void main(String[] args) {
+
+        //Create list to hold our people
+        ArrayList<Person> myPeople = new ArrayList<>();
 
         Person somePerson = new Person("Chris", 150, 100);
         Superhero someSuperhero = new Superhero("Super Chris", 200, 50);
         SuperVillain someVillain = new SuperVillain("Evil Lovi", 250, 65);
+
+        myPeople.add(someVillain);
+        myPeople.add(somePerson);
+        myPeople.add(someSuperhero);
+
+        for (Person person : myPeople) {
+
+            //For checking an instance of a specific class
+            if (person instanceof Superhero) {
+                Superhero theHero = (Superhero) person;
+                System.out.println("This was a superhero with " + theHero.getPowerLevel() + " power.");
+            }
+            if (person instanceof SuperVillain) {
+                SuperVillain theVillian = (SuperVillain) person;
+                System.out.println("This was a villain with " + theVillian.getEvilnessLevel() + " evilness");
+            }
+        }
 
         System.out.println(somePerson.getStatus());
         System.out.println(someSuperhero.getStatus());
